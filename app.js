9fa57ae01946c6a8ad5c14477bd095be67f1f434
ahmed40keseli <krkkseli@gmail.com>
2024-03-27 17:10:42 +0300

    I made a backend architectural arrangement

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
 
+// const bodyParser = require('body-parser');
+// const path = require('path');
+
+// const errorController = require('./controllers/errors');
 const sequelize = require('./util/database');
+
+// const adminRoutes = require('./routes/admin');
+// const userRoutes = require('./routes/shop');    
+
 
 const User = require("./models/user");
 const Board = require('./models/board');
 const List = require('./models/list');
 const Card = require('./models/card');
 
+// app.use('/admin', adminRoutes);
+// app.use(userRoutes);
 
 sequelize.sync()
     .then(result => {
